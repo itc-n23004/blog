684a93f7ed08d4f8a8845cb62f0190cb5f4f8ec3
itc-n23004 <n23004@std.it-colloege>
2023-12-14 06:27:56 +0000

    p54-p55 finished
    layout.jsを作成、Layoutコンポーネントに置き換える

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Header from 'components/header'
 import Hero from 'components/hero'
-import Footer from 'components/footer'
 const Home = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-      <Footer />
-    </>
+       	<Layout>
+	  <Hero />
+	</Layout>
   )
 }
 export default Home
