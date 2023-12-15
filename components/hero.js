11a754ff5d5c2640acd66fc47dc6f3e04fced9bf
itc-n23004 <n23004@std.it-colloege>
2023-12-15 01:05:22 +0000

    p63 finished
    [画像]を表示

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,8 @@
-const Hero = () => {
-  return (
-    <div>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
-  )
-}
+const Hero = ({ title, subtitle, imageOn = false }) => (
+  <div>
+    <h1>CUBE</h1>
+    <p>アウトプットしていくサイト</p>
+    {imageOn && <figure> [画像] </figure>}
+  </div>
+)
 export default Hero
