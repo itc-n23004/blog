65f4b322fbd50d136ed77eec723d6ff8f6b8288b
itc-n23004 <n23004@std.it-colloege>
2024-01-07 05:43:56 +0000

    6.4 finished
    サイトに関する情報を共有できる形で用意する

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Meta from 'components/meta'
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 const Cube = () => {
-  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+  return (
+    <Container>
+      <Meta />
+      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+    </Container>
+  )
 }
 export default Cube
