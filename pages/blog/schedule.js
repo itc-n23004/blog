7461b169726983eee56b354ec78c22774b9b6632
itc-n23004 <n23004@std.it-colloege>
2024-01-10 06:06:42 +0000

    7.6 finished
    記事ページ必要なデータを取得する関数を用意する

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,14 +1,39 @@
+import { getPostByslug } from 'lib/api'
 import { client } from 'lib/api'
 const Schedule = () => {
   return <h1>記事のタイトル</h1>
 }
-export default Schedule
+export default Schedulei({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+}) {
+  return (
+    <Container>
+        <h1>{title}</h1>
+    <Container>
+  )
+}
 
 const getStaticProps = async () => {
   const resPromise = client.get({
     endpoint: 'blogs'
   })
 
+const getPostBySlug(slug)
+
+  return {
+    props: {
+      title: post.title,
+      publish: post.publishDete,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories,
+    },
+  }
+}
   try {
     const res = await resPromise
     console.log(res)
