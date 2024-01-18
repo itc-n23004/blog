e7f1c3408f103b476c92ba05ca1c98b0115383da
itc-n23004 <n23004@std.it-colloege>
2024-01-18 07:07:28 +0000

    9.2 finished
    Dynamic Routesで記事ページを生成する

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -8,6 +8,9 @@ import {
   TwoColumnSidebar
 } from 'components/two-column'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 import PostCategories from 'components/post-categories'
 import ConvertBody from 'components/convert-body'
 import { extractText } from 'lib/extract-text'
@@ -41,6 +44,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -61,16 +66,20 @@ const Schedule = ({
 export default Schedule
 
 export async function getStaticProps () {
-  const slug = 'schedule'
+  const slug = 'micro'
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch,
       categories: post.categories,
       dedcription: description
     }
